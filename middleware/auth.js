e66c5e6cc58345a880d2b92e68c18803e604e8c3
EsraaFarhat <esraafarhat97@gmail.com>
2021-06-08 21:45:04 +0200

    Validate the input in users routes

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,13 +3,13 @@ const config = require("config");
 
 module.exports = function (req, res, next) {
   const token = req.header("x-auth-token");
-  if (!token) return res.status(401).send("Access denied. No token provided.");
+  if (!token) return res.status(401).send({message: "Access denied. No token provided."});
 
   try {
-    const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+    const decoded = jwt.verify(token, process.env.jwtPrivateKey);
     req.user = decoded;
     next();
   } catch (ex) {
-    res.status(400).send("Invalid token.");
+    res.status(400).send({message: "Invalid token."});
   }
 };
---
+++ middleware/isAdmin.js
@@ -0,0 +1,6 @@
+
+module.exports = function (req, res, next) {
+    if(req.user.role != 'admin') return res.status(403).send({message: 'Access denied.'});
+    
+    next();
+}
